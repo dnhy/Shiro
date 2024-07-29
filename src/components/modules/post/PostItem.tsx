668dd74c32a86ed4c3e1c1e80cdf6d4c31631242
dnhy <1240791903@qq.com>
2024-07-29 18:55:03 +0800

    modify list style

--- src/components/modules/post/PostItem.tsx
+++ src/components/modules/post/PostItem.tsx
@@ -51,12 +51,12 @@ export const PostItem = memo<{ data: PostModel }>(function PostItem({ data }) {
         </div>
       </div>
 
-      <div className="post-meta-bar mt-2 flex select-none flex-wrap items-center justify-end gap-4 text-base-content/60">
+      <div className="post-meta-bar  flex select-none flex-wrap items-center justify-end gap-4 text-base-content/60">
         <PostMetaBar meta={data} />
-        <span className="flex shrink-0 select-none items-center space-x-1 text-right text-accent hover:text-accent [&>svg]:hover:ml-2">
+        {/* <span className="flex shrink-0 select-none items-center space-x-1 text-right text-accent hover:text-accent [&>svg]:hover:ml-2">
           <span>阅读全文</span>
           <i className="icon-[mingcute--arrow-right-line] text-lg transition-[margin]" />
-        </span>
+        </span> */}
       </div>
     </Link>
   )
